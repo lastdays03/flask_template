64eb3da84a119e20d51f468459732dea1764c922
manigun03 <manigun03@gmail.com>
2026-01-09 14:50:31 +0900

    feat: add Docker configuration

---
+++ docker/gunicorn.conf.py
@@ -0,0 +1,35 @@
+"""Gunicorn configuration."""
+import multiprocessing
+
+# Server socket
+bind = '0.0.0.0:5000'
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+threads = 2
+timeout = 60
+keepalive = 2
+
+# Logging
+accesslog = '/app/logs/access.log'
+errorlog = '/app/logs/error.log'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'flask_app'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
